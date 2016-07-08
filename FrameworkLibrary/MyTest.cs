1108147ed4a68d897a9cb0a13f2bd1402d69f114
Brad Rembielak <BradRembielak@live.com>
2016-07-08 11:48:44 -0400

    Initial Commit

---
+++ FrameworkLibrary/MyTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FrameworkLibrary
+{
+    public class MyTest
+    {
+        public string HelloWorld()
+        {
+            return $"Hello, World! {DateTime.Now.ToString()}";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+#ASP.NET Core Web Project with Framework Library#
+
+This Visual Studio solution shows an example of referencing a .NET Framework project in a new ASP.NET Core website.
